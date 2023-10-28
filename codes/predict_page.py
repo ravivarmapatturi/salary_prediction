351a3de8a6ff5e80728ef84db5184ee55f47e224
ravivarmapatturi <132876010+ravivarmapatturi@users.noreply.github.com>
2023-10-28 16:40:20 +0530

    Update predict_page.py

--- codes/predict_page.py
+++ codes/predict_page.py
@@ -5,9 +5,9 @@ import numpy as np
 
 
 def load_model():
-    model=joblib.load(r"C:\Users\raviv\OneDrive\Documents\learning\ml\salary_prediction\models\best_model.pkl")
-    le_country=joblib.load(r"C:\Users\raviv\OneDrive\Documents\learning\ml\salary_prediction\codes\le_country.pkl")
-    le_education=joblib.load(r"C:\Users\raviv\OneDrive\Documents\learning\ml\salary_prediction\codes\le_education.pkl")
+    model=joblib.load(r"models\best_model.pkl")
+    le_country=joblib.load(r"codes\le_country.pkl")
+    le_education=joblib.load(r"codes\le_education.pkl")
     return model,le_country,le_education
 
 model,le_country,le_education=load_model()
